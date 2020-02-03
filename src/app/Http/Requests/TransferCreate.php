efd2c0d2b17012714c3b4c6ac195f99b0fa4b6b6
Serhii Kushnir <skushnir@justcoded.com>
2020-02-03 12:02:36 +0200

    move new features

--- src/app/Http/Requests/TransferCreate.php
+++ src/app/Http/Requests/TransferCreate.php
@@ -3,12 +3,39 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Str;
 
+/**
+ * Class TransferCreate
+ * @package App\Http\Requests
+ */
 class TransferCreate extends FormRequest
 {
+    /**
+     * @return array
+     */
     public function rules()
     {
         return [];
+    }
+
+    /**
+     * @return array
+     */
+    public function getData()
+    {
+        return [
+            'transferId' => Str::uuid(),
+            'payeeFsp' => 'payeefsp',
+            'payerFsp' => 'payerfsp',
+            'amount' => [
+                'amount' => '2',
+                'currency' => 'USD'
+            ],
+            'ilpPacket' => 'AQAAAAAAAABkEGcuZXdwMjEuaWQuODAwMjCCAhd7InRyYW5zYWN0aW9uSWQiOiJmODU0NzdkYi0xMzVkLTRlMDgtYThiNy0xMmIyMmQ4MmMwZDYiLCJxdW90ZUlkIjoiOWU2NGYzMjEtYzMyNC00ZDI0LTg5MmYtYzQ3ZWY0ZThkZTkxIiwicGF5ZWUiOnsicGFydHlJZEluZm8iOnsicGFydHlJZFR5cGUiOiJNU0lTRE4iLCJwYXJ0eUlkZW50aWZpZXIiOiIyNTYxMjM0NTYiLCJmc3BJZCI6IjIxIn19LCJwYXllciI6eyJwYXJ0eUlkSW5mbyI6eyJwYXJ0eUlkVHlwZSI6Ik1TSVNETiIsInBhcnR5SWRlbnRpZmllciI6IjI1NjIwMTAwMDAxIiwiZnNwSWQiOiIyMCJ9LCJwZXJzb25hbEluZm8iOnsiY29tcGxleE5hbWUiOnsiZmlyc3ROYW1lIjoiTWF0cyIsImxhc3ROYW1lIjoiSGFnbWFuIn0sImRhdGVPZkJpcnRoIjoiMTk4My0xMC0yNSJ9fSwiYW1vdW50Ijp7ImFtb3VudCI6IjEwMCIsImN1cnJlbmN5IjoiVVNEIn0sInRyYW5zYWN0aW9uVHlwZSI6eyJzY2VuYXJpbyI6IlRSQU5TRkVSIiwiaW5pdGlhdG9yIjoiUEFZRVIiLCJpbml0aWF0b3JUeXBlIjoiQ09OU1VNRVIifSwibm90ZSI6ImhlaiJ9',
+            'condition' => 'otTwY9oJKLBrWmLI4h0FEw4ksdZtoAkX3qOVAygUlTI',
+            'expiration' => '2020-10-05T14:48:00.000Z',
+        ];
     }
 }
 
