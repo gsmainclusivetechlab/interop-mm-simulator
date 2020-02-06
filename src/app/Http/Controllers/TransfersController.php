547d66193a082b7e41822914843027b9af1ffde8
Serhii Kushnir <skushnir@justcoded.com>
2020-02-06 18:19:53 +0200

    add traceparent header

--- src/app/Http/Controllers/TransfersController.php
+++ src/app/Http/Controllers/TransfersController.php
@@ -26,31 +26,28 @@ class TransfersController extends Controller
      */
     public function store(TransferCreate $request)
     {
-        $headers = [
-            'Accept'             => 'application/vnd.interoperability.transfers+json;version=1',
-            'Content-Type'       => 'application/vnd.interoperability.transfers+json;version=1.0',
-            'Date'               => date('D, d M Y H:i:s') . ' GMT',
-            'FSPIOP-Source'      => 'payerfsp',
-            'FSPIOP-Destination' => 'payeefsp',
-            'FSPIOP-Signature'   => '{"signature":"iU4GBXSfY8twZMj1zXX1CTe3LDO8Zvgui53icrriBxCUF_wltQmnjgWLWI4ZUEueVeOeTbDPBZazpBWYvBYpl5WJSUoXi14nVlangcsmu2vYkQUPmHtjOW-yb2ng6_aPfwd7oHLWrWzcsjTF-S4dW7GZRPHEbY_qCOhEwmmMOnE1FWF1OLvP0dM0r4y7FlnrZNhmuVIFhk_pMbEC44rtQmMFv4pm4EVGqmIm3eyXz0GkX8q_O1kGBoyIeV_P6RRcZ0nL6YUVMhPFSLJo6CIhL2zPm54Qdl2nVzDFWn_shVyV0Cl5vpcMJxJ--O_Zcbmpv6lxqDdygTC782Ob3CNMvg\\",\\"protectedHeader\\":\\"eyJhbGciOiJSUzI1NiIsIkZTUElPUC1VUkkiOiIvdHJhbnNmZXJzIiwiRlNQSU9QLUhUVFAtTWV0aG9kIjoiUE9TVCIsIkZTUElPUC1Tb3VyY2UiOiJPTUwiLCJGU1BJT1AtRGVzdGluYXRpb24iOiJNVE5Nb2JpbGVNb25leSIsIkRhdGUiOiIifQ"}',
-        ];
-        $client = new Client();
-
-        try {
+        app()->terminating(function() use ($request) {
+            $client = new Client();
             $client->request(
                 'POST',
                 Env::get('HOST_ML_API_ADAPTER') . '/transfers',
                 [
-                    'headers' => $headers,
+                    'headers' => [
+                        'traceparent'        => $request->header('traceparent'),
+                        'Accept'             => 'application/vnd.interoperability.transfers+json;version=1',
+                        'Content-Type'       => 'application/vnd.interoperability.transfers+json;version=1.0',
+                        'Date'               => date('D, d M Y H:i:s') . ' GMT',
+                        'FSPIOP-Source'      => 'payerfsp',
+                        'FSPIOP-Destination' => 'payeefsp',
+                        'FSPIOP-Signature'   => '{"signature":"iU4GBXSfY8twZMj1zXX1CTe3LDO8Zvgui53icrriBxCUF_wltQmnjgWLWI4ZUEueVeOeTbDPBZazpBWYvBYpl5WJSUoXi14nVlangcsmu2vYkQUPmHtjOW-yb2ng6_aPfwd7oHLWrWzcsjTF-S4dW7GZRPHEbY_qCOhEwmmMOnE1FWF1OLvP0dM0r4y7FlnrZNhmuVIFhk_pMbEC44rtQmMFv4pm4EVGqmIm3eyXz0GkX8q_O1kGBoyIeV_P6RRcZ0nL6YUVMhPFSLJo6CIhL2zPm54Qdl2nVzDFWn_shVyV0Cl5vpcMJxJ--O_Zcbmpv6lxqDdygTC782Ob3CNMvg\\",\\"protectedHeader\\":\\"eyJhbGciOiJSUzI1NiIsIkZTUElPUC1VUkkiOiIvdHJhbnNmZXJzIiwiRlNQSU9QLUhUVFAtTWV0aG9kIjoiUE9TVCIsIkZTUElPUC1Tb3VyY2UiOiJPTUwiLCJGU1BJT1AtRGVzdGluYXRpb24iOiJNVE5Nb2JpbGVNb25leSIsIkRhdGUiOiIifQ"}',
+                    ],
                     'json' => $request->getData(),
                     'debug' => true,
                 ]
             );
+        });
 
-            return $request->all();
-        } catch (BadResponseException $e) {
-            return $e->getResponse()->getBody()->getContents();
-        }
+        return $request->all();
     }
 
 
