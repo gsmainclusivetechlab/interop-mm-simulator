856e2ef65e5ed334257b73f037dcbdb294b037ad
Sergey <ssenokosov@justcoded.co>
2020-03-20 19:18:50 +0200

    Fix for last migration

--- src/database/migrations/2020_03_20_170938_add_amount_and_currency_to_transactions_table.php
+++ src/database/migrations/2020_03_20_170938_add_amount_and_currency_to_transactions_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddColumnsToTransactionsTable extends Migration
+class AddAmountAndCurrencyToTransactionsTable extends Migration
 {
     /**
      * Run the migrations.
