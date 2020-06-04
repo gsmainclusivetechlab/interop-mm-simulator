e6aeada9ba59d9231fd6721285ed822de0dfdee9
Christopher Little <christopher@littlehq.uk>
2020-06-04 13:57:21 +0100

    Increase coverage with smoke test

--- src/tests/Unit/QuotesControllerTest.php
+++ src/tests/Unit/QuotesControllerTest.php
@@ -7,18 +7,38 @@ use Tests\TestCase;
 class QuotesControllerTest extends TestCase
 {
     /**
+     * Test update.
+     *
+     * @return void
+     */
+    public function testUpdate()
+    {
+        $this->put(
+            route('quotes.update', ['id' => '+123123123']),
+            [
+                'errorInformation' => [
+                    'errorCode' => '1234',
+                    'errorDescription' => 'errorDescription',
+                ],
+            ])->assertStatus(302);
+
+    }
+
+    /**
      * Test error.
      *
      * @return void
      */
     public function testError()
     {
-        $response = $this->put(route('quotes.error', ['id' => '+123123123']), [
-            'errorInformation' => [
-                'errorCode' => '1234',
-                'errorDescription' => 'errorDescription',
-            ],
-        ]);
+        $response = $this->put(
+            route('quotes.error', ['id' => '+123123123']),
+            [
+                'errorInformation' => [
+                    'errorCode' => '1234',
+                    'errorDescription' => 'errorDescription',
+                ],
+            ]);
 
         $response->assertStatus(200);
     }
