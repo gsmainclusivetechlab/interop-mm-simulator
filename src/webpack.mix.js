6f665f27b9620d4c46b9e31013446bc52ce10b60
Samy Bouchlaghem <sbouchlaghem@gsma.com>
2020-06-19 14:20:23 +0100

    Codacy changes

--- src/webpack.mix.js
+++ src/webpack.mix.js
@@ -11,7 +11,7 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.setPublicPath('public/assets');
-mix.copyDirectory('resources/fonts', 'public/assets/fonts');
-mix.js('resources/js/app.js', 'js');
-mix.sass('resources/sass/app.scss', 'css');
+mix.setPublicPath("public/assets");
+mix.copyDirectory("resources/fonts", "public/assets/fonts");
+mix.js("resources/js/app.js", "js");
+mix.sass("resources/sass/app.scss", "css");
